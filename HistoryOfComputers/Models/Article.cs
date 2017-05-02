4b3fa8b5bf3503c0adc0311459225ee809e1b80b
Emily Allain <emilyjacquelineallain@gmail.com>
2017-05-02 14:24:41 -0300

    Changed stuff

---
+++ HistoryOfComputers/Models/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HistoryOfComputers.Models
+{
+    public class Article
+    { 
+        public int ArticleID { get; set; } //PK
+        public int PeriodID { get; set; } //FK
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string References { get; set; }
+        public string Image { get; set; }
+
+        
+    }
+}
---
+++ HistoryOfComputers/Models/TimePeriod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HistoryOfComputers.Models
+{
+    public class TimePeriod
+    {
+        public int PeriodID { get; set; } //PK
+        public string PeriodName { get; set; }
+        public virtual ICollection<Article> Articles { get; set; }
+    }
+}
